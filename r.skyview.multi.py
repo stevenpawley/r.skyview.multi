d6148f57a357cc167a0f0f4df7f6f94c09387777
Steven Pawley <dr.stevenpawley@gmail.com>
2021-12-02 22:56:17 -0700

    Update r.skyview.multi.py

--- r.skyview.multi.py
+++ r.skyview.multi.py
@@ -6,14 +6,15 @@
 #
 # AUTHOR(S):    Anna Petrasova (kratochanna gmail.com)
 #               Vaclav Petras <wenzeslaus gmail com> (colorize addition)
+#               Steven Pawley <steven.pawley gmail.com> (parallelization)
 #
 # PURPOSE:      Implementation of Sky-View Factor visualization technique
 #
-# COPYRIGHT:    (C) 2013-2014 by the GRASS Development Team
+# COPYRIGHT:    (C) 2013-2021 by the GRASS Development Team
 #
-#		This program is free software under the GNU General Public
-#		License (version 2). Read the file COPYING that comes with GRASS
-#		for details.
+# 		This program is free software under the GNU General Public
+# 		License (version 2). Read the file COPYING that comes with GRASS
+# 		for details.
 #
 ##############################################################################
 
@@ -22,10 +23,12 @@
 #% keyword: raster
 #% keyword: visualization
 #%end
+
 #%option G_OPT_R_INPUT
 #%end
 #%option G_OPT_R_OUTPUT
 #%end
+
 #%option
 #% key: ndir
 #% description: Number of directions (8 to 32 recommended)
@@ -34,12 +37,14 @@
 #% answer: 16
 #% options: 2-360
 #%end
+
 #%option
 #% key: maxdistance
 #% description: The maximum distance to consider when finding the horizon height
 #% type: double
 #% required: no
 #%end
+
 #%option
 #% key: color_source
 #% type: string
@@ -52,12 +57,14 @@
 #% answer: input
 #% guisection: Colorize
 #%end
+
 #%option G_OPT_R_INPUT
 #% key: color_input
 #% required: no
 #% description: Custom raster map to be used for colorization
 #% guisection: Colorize
 #%end
+
 #%option
 #% key: color_table
 #% type: string
@@ -68,24 +75,33 @@
 #% options: reds, blues, greens, oranges, sepia, aspectcolr
 #% guisection: Colorize
 #%end
+
 #%option G_OPT_R_OUTPUT
 #% key: colorized_output
 #% required: no
 #% description: Colorized sky-view factor
 #% guisection: Colorize
 #%end
+
 #%option
 #%  key: basename
 #%  type: string
 #%  multiple: no
 #%  description: Set the basename for the intermediate maps
 #%end
+
+#% key: o
+#% label: Compute openness instead of skyview factor
+#% description: Openness considers zenith angles > 90 degrees
+#%end
+
 #%flag
 #% key: n
 #% label: Invert color table for colorization raster
 #% description: Ignored for input and color_input
 #% guisection: Colorize
 #%end
+
 #%flag
 #% key: p
 #% label: Do not process in parallel
@@ -106,78 +122,108 @@ from grass.pygrass.modules import Module, ParallelModuleQueue
 from copy import deepcopy
 
 # TODO: also used for r.slope.aspect result
-TMP_NAME = 'tmp_horizon_' + str(os.getpid())
+TMP_NAME = "tmp_horizon_" + str(os.getpid())
 CLEANUP = True
 
 
 def cleanup():
     if CLEANUP:
         gcore.verbose(_("Cleaning temporary maps..."))
-        gcore.run_command('g.remove', flags='f', type='raster',
-                          pattern=TMP_NAME + "*", quiet=True)
+        gcore.run_command(
+            "g.remove", flags="f", type="raster", pattern=TMP_NAME + "*", quiet=True
+        )
 
 
 def main():
-    elev = options['input']
-    output = options['output']
-    n_dir = int(options['ndir'])
-    notParallel = flags['p']
+    elev = options["input"]
+    output = options["output"]
+    n_dir = int(options["ndir"])
+    notParallel = flags["p"]
+
     global TMP_NAME, CLEANUP
-    if options['basename']:
-        TMP_NAME = options['basename']
+
+    if options["basename"]:
+        TMP_NAME = options["basename"]
         CLEANUP = False
-    colorized_output = options['colorized_output']
-    colorize_color = options['color_table']
+
+    colorized_output = options["colorized_output"]
+    colorize_color = options["color_table"]
+
     if colorized_output:
         color_raster_tmp = TMP_NAME + "_color_raster"
     else:
         color_raster_tmp = None
-    color_raster_type = options['color_source']
-    color_input = options['color_input']
-    if color_raster_type == 'color_input' and not color_input:
+
+    color_raster_type = options["color_source"]
+    color_input = options["color_input"]
+
+    if color_raster_type == "color_input" and not color_input:
         gcore.fatal(_("Provide raster name in color_input option"))
-    if color_raster_type != 'color_input' and color_input:
-        gcore.fatal(_("The option color_input is not needed"
-                      " when not using it as source for color"))
+
+    if color_raster_type != "color_input" and color_input:
+        gcore.fatal(
+            _(
+                "The option color_input is not needed"
+                " when not using it as source for color"
+            )
+        )
     # this would be needed only when no value would allowed
     if not color_raster_type and color_input:
-        color_raster_type = 'color_input'  # enable for convenience
-    if color_raster_type == 'aspect' \
-            and colorize_color \
-            and colorize_color not in ['default', 'aspectcolr']:
-        gcore.warning(_("Using possibly inappropriate color table <{}>"
-                        " for aspect".format(colorize_color)))
+        color_raster_type = "color_input"  # enable for convenience
 
-    horizon_step = 360. / n_dir
+    if (
+        color_raster_type == "aspect"
+        and colorize_color
+        and colorize_color not in ["default", "aspectcolr"]
+    ):
+        gcore.warning(
+            _(
+                "Using possibly inappropriate color table <{}>"
+                " for aspect".format(colorize_color)
+            )
+        )
+
+    horizon_step = 360.0 / n_dir
     horizon_intervals = np.arange(0, 360, horizon_step)
     msgr = get_msgr()
 
     # checks if there are already some maps
     old_maps = _get_horizon_maps()
+
     if old_maps:
         if not gcore.overwrite():
             CLEANUP = False
-            msgr.fatal(_("You have to first check overwrite flag or remove"
-                         " the following maps:\n"
-                         "{names}").format(names=','.join(old_maps)))
+            msgr.fatal(
+                _(
+                    "You have to first check overwrite flag or remove"
+                    " the following maps:\n"
+                    "{names}"
+                ).format(names=",".join(old_maps))
+            )
         else:
-            msgr.warning(_("The following maps will be overwritten: {names}"
-                           ).format(names=','.join(old_maps)))
+            msgr.warning(
+                _("The following maps will be overwritten: {names}").format(
+                    names=",".join(old_maps)
+                )
+            )
 
     if not gcore.overwrite() and color_raster_tmp:
         check_map_name(color_raster_tmp)
 
     try:
         if notParallel is False:
-            if options['maxdistance']:
-                maxdistance = float(options['maxdistance'])
+            if options["maxdistance"]:
+                maxdistance = float(options["maxdistance"])
             else:
                 maxdistance = None
 
             r_horizon = Module(
-                "r.horizon", elevation=elev,
-                maxdistance=maxdistance, flags='d',
-                run_=False)
+                "r.horizon",
+                elevation=elev,
+                maxdistance=maxdistance,
+                flags="d",
+                run_=False,
+            )
 
             queue = ParallelModuleQueue(nprocs=len(horizon_intervals))
 
@@ -186,70 +232,101 @@ def main():
                 r_horizon_prc.inputs.direction = d
                 r_horizon_prc.outputs.output = TMP_NAME
                 queue.put(r_horizon_prc)
+
             queue.wait()
 
         else:
             params = {}
-            if options['maxdistance']:
-                params['maxdistance'] = options['maxdistance']
-            gcore.run_command('r.horizon', elevation=elev, step=horizon_step,
-                          output=TMP_NAME, flags='d', **params)
+            if options["maxdistance"]:
+                params["maxdistance"] = options["maxdistance"]
+
+            gcore.run_command(
+                "r.horizon",
+                elevation=elev,
+                step=horizon_step,
+                output=TMP_NAME,
+                flags="d",
+                **params
+            )
 
         msgr.message(_("Computing sky view factor ..."))
         new_maps = _get_horizon_maps()
-        expr = '"{out}" = 1 - (sin("{first}") '.format(first=new_maps[0], out=output)
-        for horizon in new_maps[1:]:
-            expr += '+ sin("{name}") '.format(name=horizon)
-        expr += ") / {n}.".format(n=len(new_maps))
 
-        grast.mapcalc(exp=expr)
-        gcore.run_command('r.colors', map=output, color='grey')
+        if flags["o"]:
+            msgr.message(_("Computing openness ..."))
+            expr = "{out} = 1 - (sin({first}) ".format(first=new_maps[0], out=output)
+            for horizon in new_maps[1:]:
+                expr += "+ sin({name}) ".format(name=horizon)
+            expr += ") / {n}.".format(n=len(new_maps))
+        else:
+            msgr.message(_("Computing skyview factor ..."))
+            expr = "{out} = 1 - (sin( if({first} < 0, 0, {first}) ) ".format(
+                first=new_maps[0], out=output
+            )
+            for horizon in new_maps[1:]:
+                expr += "+ sin( if({name} < 0, 0, {name}) ) ".format(name=horizon)
+            expr += ") / {n}.".format(n=len(new_maps))
+
     except CalledModuleError:
-        msgr.fatal(_("r.horizon failed to compute horizon elevation "
-                     "angle maps. Please report this problem to developers."))
+        msgr.fatal(
+            _(
+                "r.horizon failed to compute horizon elevation "
+                "angle maps. Please report this problem to developers."
+            )
+        )
         return 1
+
     if colorized_output:
-        if color_raster_type == 'slope':
-            gcore.run_command('r.slope.aspect', elevation=elev,
-                              slope=color_raster_tmp)
-        elif color_raster_type == 'aspect':
-            gcore.run_command('r.slope.aspect', elevation=elev,
-                              aspect=color_raster_tmp)
-        elif color_raster_type == 'dxy':
-            gcore.run_command('r.slope.aspect', elevation=elev,
-                              dxy=color_raster_tmp)
-        elif color_raster_type == 'color_input':
+        if color_raster_type == "slope":
+            gcore.run_command("r.slope.aspect", elevation=elev, slope=color_raster_tmp)
+        elif color_raster_type == "aspect":
+            gcore.run_command("r.slope.aspect", elevation=elev, aspect=color_raster_tmp)
+        elif color_raster_type == "dxy":
+            gcore.run_command("r.slope.aspect", elevation=elev, dxy=color_raster_tmp)
+        elif color_raster_type == "color_input":
             color_raster_tmp = color_input
         else:
             color_raster_tmp = elev
+
         # don't modify user's color table for inputs
-        if colorize_color \
-                and color_raster_type not in ['input', 'color_input']:
-            rcolors_flags = ''
-            if flags['n']:
-                rcolors_flags += 'n'
-            gcore.run_command('r.colors', map=color_raster_tmp,
-                              color=colorize_color,
-                              flags=rcolors_flags)
-        gcore.run_command('r.shade', shade=output, color=color_raster_tmp,
-                          output=colorized_output)
+        if colorize_color and color_raster_type not in ["input", "color_input"]:
+            rcolors_flags = ""
+
+            if flags["n"]:
+                rcolors_flags += "n"
+
+            gcore.run_command(
+                "r.colors",
+                map=color_raster_tmp,
+                color=colorize_color,
+                flags=rcolors_flags,
+            )
+
+        gcore.run_command(
+            "r.shade", shade=output, color=color_raster_tmp, output=colorized_output
+        )
         grast.raster_history(colorized_output)
+
     grast.raster_history(output)
+
     return 0
 
 
 def _get_horizon_maps():
-    return gcore.list_grouped('rast',
-                              pattern=TMP_NAME + "*")[gcore.gisenv()['MAPSET']]
+    return gcore.list_grouped("rast", pattern=TMP_NAME + "*")[gcore.gisenv()["MAPSET"]]
 
 
 def check_map_name(name):
     # cell means any raster in this context
     # mapset needs to retrieved in very call, ok for here
-    if gcore.find_file(name, element='cell',
-                       mapset=gcore.gisenv()['MAPSET'])['file']:
-        gcore.fatal(_("Raster map <%s> already exists. "
-                      "Remove the existing map or allow overwrite.") % name)
+    if gcore.find_file(name, element="cell", mapset=gcore.gisenv()["MAPSET"])["file"]:
+        gcore.fatal(
+            _(
+                "Raster map <%s> already exists. "
+                "Remove the existing map or allow overwrite."
+            )
+            % name
+        )
 
 
 if __name__ == "__main__":
